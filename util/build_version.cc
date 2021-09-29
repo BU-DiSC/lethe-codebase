bec3bd3221deade8f1c88116457c6167aa295e3e
Subhadeep Sarkar <subhadeepsarkarybs@yahoo.com>
2021-09-29 16:32:50 -0400

    Initializing repo

---
+++ util/build_version.cc
@@ -0,0 +1,5 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:10592ede52ab2e54b157794f2ec9bee807c59f43";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-11-11";
+const char* rocksdb_build_compile_date = __DATE__;
